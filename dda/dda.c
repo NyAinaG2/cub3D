93b0656057c24546170e02038951c1c7482b8d7c
mrakotos <mrakotos@student.42antananarivo.mg>
2026-05-03 16:14:54 +0300

    feat: one ray dda

--- dda/dda.c
+++ dda/dda.c
@@ -6,22 +6,30 @@
 /*   By: mrakotos <mrakotos@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/03 09:27:35 by mrakotos          #+#    #+#             */
-/*   Updated: 2026/05/03 15:23:32 by mrakotos         ###   ########.fr       */
+/*   Updated: 2026/05/03 16:06:46 by mrakotos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "dda.h"
 
 t_point has_hit(t_point p, float dir)
-
 {
+	t_point tmp;
 	t_point delta;
 	t_point side;
 	t_point angle;
 	t_point step;
 	t_point res;
 	float t;
+	int hit;
+	int mapX;
+	int mapY;
 
+	hit = 0;
+	mapX = (int)p.x;
+	mapY = (int)p.y;
+	tmp.x = p.x;
+	tmp.y = p.y;
 	angle.x = cos(dir);
 	angle.y = sin(dir);
 	step.x = copysign(1.0, angle.x);
@@ -36,10 +44,22 @@ t_point has_hit(t_point p, float dir)
 		side.y = (floor(p.y + 1) - p.y) * delta.y;
 	else
 		side.y = (p.y - floor(p.y)) * delta.y;
-	if (side.x > side.y)
-		t = side.y;
-	else
-		t = side.x;
+	while (hit == 0)
+	{
+		if (side.x < side.y)
+		{
+			t = side.x;
+			side.x += delta.x;
+			mapX += (int)step.x;
+		}
+		else
+		{
+			t = side.y;
+			side.y += delta.y;
+			mapY += (int)step.y;
+		}
+		if (tmp_map[mapY][mapX] == 1) hit = 1;
+	}
 	res.x = p.x + t * angle.x;
 	res.y = p.y + t * angle.y;
 	return (res);
