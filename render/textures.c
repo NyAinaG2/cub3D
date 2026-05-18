e15dc29f5f165f45504c9204b20d107233b7bc1f
mrakotos <mrakotos@student.42antananarivo.mg>
2026-05-18 20:48:43 +0300

    fix: wrong float calculation for y

--- render/textures.c
+++ render/textures.c
@@ -27,12 +27,13 @@ int	sample_texture(t_texture *texture, int x, int y)
 
 int	get_texture_y(t_texture *texture, int current_y, int start, int len_column)
 {
-	int	res;
+	float	u;
+	int		res;
 
 	if (len_column <= 0)
 		return (0);
-	res = (float)(current_y - start) / (float)len_column;
-	res = (int)(res * texture->size_y);
+	u = (float)(current_y - start) / (float)len_column;
+	res = (int)(u * texture->size_y);
 	if (res < 0)
 		res = 0;
 	if (res >= texture->size_y)
@@ -44,11 +45,11 @@ int	get_texture_x(t_texture *texture, t_params *params)
 {
 	int	res;
 
-	if (params->scale == 1)
-		return (texture->size_x - 1);
-	if (params->scale == 0)
-		return (0);
 	res = (int)(params->scale * texture->size_x);
+	if (res < 0)
+		res = 0;
+	if (res >= texture->size_x)
+		res = texture->size_x - 1;
 	return (res);
 }
 
@@ -68,22 +69,25 @@ void	draw_column(int len, int x, t_test *data, t_params *params)
 {
 	int			start;
 	int			end;
+	int			start_unclipped;
+	int			y;
 	t_column	column;
 
 	column.len = len;
-	start = -len / 2 + WIN_H / 2;
+	start_unclipped = -len / 2 + WIN_H / 2;
+	start = start_unclipped;
 	end = len / 2 + WIN_H / 2;
-	column.current = start;
 	if (start < 0)
 		start = 0;
 	if (end >= WIN_H)
 		end = WIN_H - 1;
-	column.start = start;
-	while (start < end)
+	column.start = start_unclipped;
+	y = start;
+	while (y < end)
 	{
-		put_pixel_to_img(data, x, start, get_texture_color(params, &data->north,
+		column.current = y;
+		put_pixel_to_img(data, x, y, get_texture_color(params, &data->north,
 				&column));
-		column.current++;
-		start++;
+		y++;
 	}
 }
