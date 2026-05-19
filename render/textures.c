f42f5b3ddb84dfd0d76311e367f75fe2f44fc7f6
mrakotos <mrakotos@student.42antananarivo.mg>
2026-05-19 17:23:53 +0300

    feat: remove divison from y loop

--- render/textures.c
+++ render/textures.c
@@ -6,7 +6,7 @@
 /*   By: mrakotos <mrakotos@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 17:37:13 by mrakotos          #+#    #+#             */
-/*   Updated: 2026/05/19 16:34:07 by mrakotos         ###   ########.fr       */
+/*   Updated: 2026/05/19 17:22:14 by mrakotos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,13 @@ static int	sample_texture(t_texture *texture, int x, int y)
 	return (*(unsigned int *)addr);
 }
 
-static int	get_texture_y(t_texture *texture, int current_y, int start,
-		int len_column)
+static int	get_texture_y(t_texture *texture, t_column *column)
 {
-	float	u;
-	int		res;
+	int	res;
 
-	if (len_column <= 0)
+	if (column->len <= 0)
 		return (0);
-	u = (float)(current_y - start) / (float)len_column;
-	res = (int)(u * texture->size_y);
+	res = (int)(column->y_pos * texture->size_y);
 	if (res < 0)
 		res = 0;
 	if (res >= texture->size_y)
@@ -64,7 +61,7 @@ static int	get_texture_color(t_texture *texture, t_column *column)
 	int	y;
 	int	res;
 
-	y = get_texture_y(texture, column->current, column->start, column->len);
+	y = get_texture_y(texture, column);
 	res = sample_texture(texture, column->x_texture_img, y);
 	return (res);
 }
@@ -75,12 +72,16 @@ void	draw_column(int len, int x, t_test *data, t_params *params)
 	int			end;
 	int			y;
 	t_column	column;
+	t_texture	*texture;
 
+	texture = get_texture(params, data);
 	column.len = len;
 	column.start = (WIN_H - len) / 2;
 	start = column.start;
 	end = column.start + len;
-	column.x_texture_img = get_texture_x(get_texture(params, data), params);
+	column.x_texture_img = get_texture_x(texture, params);
+	column.y_pos = 0;
+	column.y_step = (float)texture->size_y / (float)column.len;
 	if (start < 0)
 		start = 0;
 	if (end > WIN_H)
@@ -89,8 +90,8 @@ void	draw_column(int len, int x, t_test *data, t_params *params)
 	while (y < end)
 	{
 		column.current = y;
-		put_pixel_to_img(data, x, y, get_texture_color(get_texture(params,
-					data), &column));
+		put_pixel_to_img(data, x, y, get_texture_color(texture, &column));
+		column.y_pos += column.y_step;
 		y++;
 	}
 }
