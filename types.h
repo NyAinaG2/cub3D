9101e2a4591109b4b8be67b8e201bc0b5637d33c
mrakotos <mrakotos@student.42antananarivo.mg>
2026-05-20 11:59:35 +0300

    chore: remove wall color in t_game

--- types.h
+++ types.h
@@ -6,133 +6,132 @@
 /*   By: andrrand <andrrand@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/03 09:01:01 by mrakotos          #+#    #+#             */
-/*   Updated: 2026/05/20 11:37:27 by mrakotos         ###   ########.fr       */
+/*   Updated: 2026/05/20 11:58:46 by mrakotos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
-#define TYPES_H
+# define TYPES_H
 
-#define WIN_W 680
-#define WIN_H 480
-#define WIN_TITLE "cub3D"
-#define ROTATION_SPEED 3
-#define MOVE_SPEED 3
-#define ESC 65307
-#define KEY_W 119
-#define KEY_A 97
-#define KEY_D 100
-#define KEY_S 115
-#define KEY_LEFT 65361
-#define KEY_RIGHT 65363
-#include "./parse/parse.h"
+# define WIN_W 680
+# define WIN_H 480
+# define WIN_TITLE "cub3D"
+# define ROTATION_SPEED 3
+# define MOVE_SPEED 3
+# define ESC 65307
+# define KEY_W 119
+# define KEY_A 97
+# define KEY_D 100
+# define KEY_S 115
+# define KEY_LEFT 65361
+# define KEY_RIGHT 65363
+# include "./parse/parse.h"
 
 typedef struct s_move
 {
-	int rotate_left;
-	int rotate_right;
-	int forward;
-	int backward;
-	int left;
-	int right;
-} t_move;
+	int			rotate_left;
+	int			rotate_right;
+	int			forward;
+	int			backward;
+	int			left;
+	int			right;
+}				t_move;
 
 typedef struct s_texture
 {
-	int size_x;
-	int size_y;
-	int line_length;
-	int bpp;
-	int endian;
-	char* addr;
-	void* img;
-} t_texture;
+	int			size_x;
+	int			size_y;
+	int			line_length;
+	int			bpp;
+	int			endian;
+	char		*addr;
+	void		*img;
+}				t_texture;
 
 typedef struct game_s
 {
-	t_move move;
-	void* mlx_ptr;
-	void* windows_ptr;
-	char** map;
-	t_texture north;
-	t_texture east;
-	t_texture south;
-	t_texture west;
-	float px;
-	float py;
-	float player_box;
-	float direction;
-	float fov_max;
-	float fov_min;
-	float fov;
-	float step;
-	double game_time;
-	double last_frame;
-	int wall_color;
-	int ceil_color;
-	int floor_color;
-	int map_size_x;
-	int map_size_y;
-	void* img;
-	char* addr;
-	int bpp;
-	int line_length;
-	int endian;
-	t_data* parse;
-} t_game;
+	t_move		move;
+	void		*mlx_ptr;
+	void		*windows_ptr;
+	char		**map;
+	t_texture	north;
+	t_texture	east;
+	t_texture	south;
+	t_texture	west;
+	float		px;
+	float		py;
+	float		player_box;
+	float		direction;
+	float		fov_max;
+	float		fov_min;
+	float		fov;
+	float		step;
+	double		game_time;
+	double		last_frame;
+	int			ceil_color;
+	int			floor_color;
+	int			map_size_x;
+	int			map_size_y;
+	void		*img;
+	char		*addr;
+	int			bpp;
+	int			line_length;
+	int			endian;
+	t_data		*parse;
+}				t_game;
 
 typedef struct s_point
 {
-	float x;
-	float y;
-} t_point;
+	float		x;
+	float		y;
+}				t_point;
 
 typedef struct s_column
 {
-	t_texture* texture;
-	float step;
-	int start;
-	int current;
-	int len;
-	float x_texture_img;
-	float y_pos;
-	float y_step;
-} t_column;
+	t_texture	*texture;
+	float		step;
+	int			start;
+	int			current;
+	int			len;
+	float		x_texture_img;
+	float		y_pos;
+	float		y_step;
+}				t_column;
 
 typedef struct s_vec
 {
-	t_point delta;
-	t_point side;
-	t_point angle;
-	t_point step;
-	t_point res;
-	float t;
-	int map_x;
-	int map_y;
-	int hit;
-	char first_hit;
-} t_vec;
+	t_point		delta;
+	t_point		side;
+	t_point		angle;
+	t_point		step;
+	t_point		res;
+	float		t;
+	int			map_x;
+	int			map_y;
+	int			hit;
+	char		first_hit;
+}				t_vec;
 typedef struct s_params
 {
-	char side;
-	char direction;
-	float scale;
-	float d;
-} t_params;
+	char		side;
+	char		direction;
+	float		scale;
+	float		d;
+}				t_params;
 
 typedef struct s_dda
 {
-	t_point hit_p;
-	t_point side_d;
-	float distance;
-	int side;
-} t_dda;
+	t_point		hit_p;
+	t_point		side_d;
+	float		distance;
+	int			side;
+}				t_dda;
 
 typedef struct s_color
 {
-	int r;
-	int g;
-	int b;
-} t_color;
+	int			r;
+	int			g;
+	int			b;
+}				t_color;
 
 #endif
